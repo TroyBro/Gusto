8d70857c282ba8fdd04001da6b8bf7eb17468a07
TroyBro <SliusarYV@krok.edu.ua>
2021-02-16 12:34:50 +0200

    Initial commit

---
+++ users_messages/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from main_gusto.models import UsersMessages
+from django.core.paginator import Paginator
+
+# Create your views here.
+def messages_view(request):
+    messages = UsersMessages.objects.filter(is_processed=False).order_by('send_date')
+    paginator = Paginator(messages, 5)
+    page = request.GET.get('page')
+    messages_page = paginator.get_page(page)
+    return render(request, 'massages_view.html', context={'items': messages_page})
